c9deca1e7d8e6a5f262c5041c627666059802871
minjoo12 <hope_0726@hanmail.net>
2021-07-14 20:18:21 +0900

    edit database

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': os.environ.get('DB_NAME'),
+       'USER': os.environ.get('DB_USER'),
+       'PASSWORD': os.environ.get('DB_PASSWORD'),
+       'HOST': os.environ.get('DB_HOST'),
+       'PORT': '',
+   }
 }
 
 
